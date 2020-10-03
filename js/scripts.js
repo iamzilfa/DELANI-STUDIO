82f7b6dfec2b126dfddbb1a1f6edcd8b5cd7b348
Zilfa Cyamani <zilcyam@gmail.com>
2020-10-04 00:52:37 +0200

    what we do added in Js

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+$("#imgDes").click(function () {
+    $("#textDes").show();
+    $("#imgDes").hide();
+});
+
+$("#textDes").click(function () {
+    $("#imgDes").show();
+    $("#textDes").hide();
+});
+
+
+$("#imgDev").click(function () {
+    $("#textDev").show();
+    $("#imgDev").hide();
+});
+
+$("#textDev").click(function () {
+    $("#imgDev").show();
+    $("#textDev").hide();
+});
+
+
+$("#imgProd").click(function () {
+    $("#textProd").show();
+    $("#imgProd").hide();
+});
+
+$("#textProd").click(function () {
+    $("#imgProd").show();
+    $("#textProd").hide();
+});
